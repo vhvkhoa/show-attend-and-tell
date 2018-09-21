af399f2a8202180cfa803ea61f8bb0eb7cb963d9
Khoa Vo <vhvkhoa@gmail.com>
2018-09-21 15:18:45 +0700

    Fixed minor errors.

--- feature_extractor.py
+++ feature_extractor.py
@@ -88,7 +88,7 @@ class TensorFlowFeatureExtracter(object):
         # If resnet 101: use unit 22
         # If resnet 152: use unit 35
         unit = 22 if model_num_layers == '101' else 35 if model_num_layers == '152' else 22
-        model_name = '%s' % (model_name) + '_v2_%s' if model_name == 'resnet' else '%s' if model_name == 'vgg'
+        model_name = '%s' % (model_name) + '_v2_%s' if model_name == 'resnet' else '%s'
         model_name = model_name % (model_num_layers)
         layer_name = model_name + '/block%d/unit_%d/bottleneck_v%d' % (3, unit, 2)
         image_preprocessing_fn = preprocessing_factory.get_preprocessing('inception', is_training=False)
