34fa2c029fc88f006cd4d9821341281bb0c8c703
Khoa Vo <vhvkhoa@gmail.com>
2018-09-27 16:40:08 +0700

    Fixing tensorflow ver error.

--- core/tf_beam_decoder.py
+++ core/tf_beam_decoder.py
@@ -634,15 +634,12 @@ class BeamSearchHelper(object):
     def decode_dense(self):
         emit_ta, final_state, final_loop_state = tf.nn.raw_rnn(self.cell, self.loop_fn, scope=self.scope)
         cand_symbols, cand_logprobs, cand_finished, beam_symbols, beam_logprobs, cand_alphas, cand_betas, beam_alphas, beam_betas, beam_context = final_loop_state
+        cand_symbols = tf.transpose(cand_symbols.stack(), [1, 0])
+        cand_logprobs = tf.transpose(cand_logprobs.stack(), [1, 0])
+        cand_alphas = tf.transpose(cand_alphas.stack(), [1, 0, 2])
+        cand_betas = tf.transpose(cand_betas.stack(), [1, 0])
         return cand_symbols, cand_logprobs, cand_alphas, cand_betas 
 
-    def decode_sparse(self, include_stop_tokens=True):
-        dense_symbols, logprobs, alphas, betas = self.decode_dense()
-        mask = tf.not_equal(dense_symbols, self.stop_token)
-        if include_stop_tokens:
-            mask = tf.concat([tf.ones_like(mask[:,:1]), mask[:,:-1]], 1)
-        return sparse_boolean_mask(dense_symbols, mask), logprobs
-# %%
 
 def beam_decoder(
         cell,
